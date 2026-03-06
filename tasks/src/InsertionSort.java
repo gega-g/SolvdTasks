b5851b922dd078c65f8ad60319fba998e69a0651
gegagavasheli <gegagavasheli@gmail.com>
2026-03-06 16:41:03 +0400

    First task

---
+++ tasks/src/InsertionSort.java
@@ -0,0 +1,26 @@
+public class InsertionSort {
+
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {9, 5, 1, 4, 3};
+
+        System.out.println("Before:");
+        for(int n : data) System.out.print(n + " ");
+
+        sort(data);
+
+        System.out.println("\nAfter:");
+        for(int n : data) System.out.print(n + " ");
+    }
+}
